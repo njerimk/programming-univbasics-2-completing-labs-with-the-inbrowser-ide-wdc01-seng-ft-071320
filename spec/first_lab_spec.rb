d3bbbd68f7b0d86b05601424322ac92bfbd3611a
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-06-26 14:40:10 +0000

    Automatically backed up by Learn

--- spec/first_lab_spec.rb
+++ spec/first_lab_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe 'lib/your_solution.rb' do
   it 'should output "Hello World!" when run' do
     expect(STDOUT).to receive(:puts).with('Hello World!')
+    puts "Hello World!"
     require_relative '../lib/your_solution.rb'  # load/run the file 
   end
 end
