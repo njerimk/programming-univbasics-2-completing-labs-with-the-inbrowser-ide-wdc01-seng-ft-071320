6dc36e1362e62053f99a49d5217987ccfd0dfb10
Njeri W Murugi-Kamau <nmurugikamau@gmail.com>
2020-06-26 14:45:37 +0000

    Automatically backed up by Learn

--- lib/your_solution.rb
+++ lib/your_solution.rb
@@ -1,6 +1,6 @@
 
 # Write your solution here
-~/lib/your_solution.rb = "Hello World!"
+~/programming-univbasics-2-completing-labs-with-the-inbrowser-ide-wdc01-seng-ft-071320/lib/your_solution.rb = "Hello World!"
 puts "Hello World!
 
 # This file should output "Hello World!" using a puts statement
